186b02f8f7c1812f60aa621090a572f5d20f6a68
marty-hope <R1chardy0rk>
2016-11-21 14:08:56 -0800

    Add simple reduce function

--- js/app.tsx
+++ js/app.tsx
@@ -1,10 +1,24 @@
-declare var fetch;
+import * as React from 'react';
+import { createStore } from 'redux';
 
-fetch('http://localhost:3010/widgets')
-    .then(res => res.json())
-    .then(results => {
-        console.log(results);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+var actions = [
+    {type: 'add', value: 1 },
+    {type: 'add', value: 2 },
+    {type: 'subtract', value: 3 },
+    {type: 'subtract', value: 5 },
+    {type: 'add', value: 20 },
+];
+
+var final =actions.reduce( (state, action) => {
+    switch(action.type)
+    {
+        case 'add':
+            return Object.assign({}, state, {sum: state.sum + action.value});
+        case 'subtract':
+            return Object.assign({}, state, {sum: state.sum - action.value});
+        default:
+            return state;
+    }
+}, {sum: 0});
+
+console.log(final);
